a8da3b64a8e52f27ca1e20345398223bfcf053f2
Roberto Olivares <somacky@gmail.com>
2017-12-12 22:32:00 -0600

    Commit general

---
+++ Contoso.API/Controllers/TestController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Contoso.API.Models;
+
+namespace Contoso.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        private readonly TestContext _testContext;
+        public TestController(TestContext context)
+        {
+            _testContext = context;
+            if (_testContext.TestItems.Count() == 0)
+            {
+                _testContext.TestItems.Add(new Models.Test { Nombre = "Test" });
+                _testContext.SaveChanges();
+            }
+
+        }
+        
+        [HttpGet]
+        public IEnumerable<Test> GetAll()
+        {   
+            return _testContext.TestItems.ToList();
+        }
+
+
+        [HttpGet("{id}")]
+        [Route("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var item = _testContext.TestItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Test item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _testContext.TestItems.Add(item);
+            _testContext.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+    }
+}
---
+++ Contoso.API/Models/TestContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contoso.API.Models
+{
+    public class TestContext : DbContext
+    {
+        public TestContext(DbContextOptions<TestContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Test> TestItems { get; set; }
+    }
+}
